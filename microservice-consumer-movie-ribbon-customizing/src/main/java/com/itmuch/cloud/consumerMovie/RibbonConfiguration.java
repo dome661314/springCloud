87114785c1df2a327acc476925cd7117ea23e2e3
zhengw <zhengw@glodon.com>
2019-10-08 16:37:35 +0800

    提交信息

---
+++ microservice-consumer-movie-ribbon-customizing/src/main/java/com/itmuch/cloud/consumerMovie/RibbonConfiguration.java
@@ -0,0 +1,17 @@
+package com.itmuch.cloud.consumerMovie;
+
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RibbonConfiguration {
+
+    @Bean
+    public IRule ribbonRule() {
+        return new RandomRule();
+    }
+}
