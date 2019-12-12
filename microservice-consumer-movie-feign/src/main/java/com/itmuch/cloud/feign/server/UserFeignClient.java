e918708a14cd285ca31374e41587161cf0a17d45
zhengw <617806166@qq.com>
2019-12-12 08:16:34 +0800

    feign提交

---
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/feign/server/UserFeignClient.java
@@ -0,0 +1,23 @@
+package com.itmuch.cloud.feign.server;
+
+import com.itmuch.cloud.feign.model.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author dome
+ */
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+
+    /**
+     * 通过id获取用户信息
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    User findById(@PathVariable("id") Long id);
+}
