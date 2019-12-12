b5c611eca3ba241c8fe457abd450a5eb3b8e4480
zhengw <617806166@qq.com>
2019-12-12 10:42:25 +0800

    feign-权限

---
+++ microservice-provider-user-with-auth/src/main/java/com/itmuch/cloud/providerUser/controller/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.itmuch.cloud.providerUser.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author dome
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 所有请求，都需要经过 HTTP basic 认证
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("admin")).roles("USER");
+    }
+}
