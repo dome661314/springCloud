3240d8dccd40209ad82b12b931d39e71e2377c30
zhengw <617806166@qq.com>
2019-12-26 21:30:13 +0800

    zuul

---
+++ microservice-file-upload/src/test/java/microservice/fileupload/MicroserviceFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFileUploadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
